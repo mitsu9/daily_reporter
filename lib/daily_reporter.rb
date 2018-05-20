84ad0ce960b0d8b372a92e2ef27d638c1c8df06f
mitsu9 <mitsunobu.h.9@gmail.com>
2018-05-21 00:59:58 +0900

    Add create method

--- lib/daily_reporter.rb
+++ lib/daily_reporter.rb
@@ -1,4 +1,5 @@
 require "daily_reporter/version"
+require "daily_reporter/cli"
 
 module DailyReporter
   # Your code goes here...
---
+++ lib/daily_reporter/cli.rb
@@ -0,0 +1,11 @@
+require "daily_reporter"
+require "thor"
+
+module DailyReporter
+  class CLI < Thor
+    desc "create", "create a daily report"
+    def create
+      puts "[WIP]create a daily report"
+    end
+  end
+end
