002667d7fc6e2c8276e49b140fbcc40e0e96ca8c
mitsu9 <mitsunobu.h.9@gmail.com>
2018-05-25 01:46:16 +0900

    オレオレ仕様の出力は成功

--- lib/daily_reporter/cli.rb
+++ lib/daily_reporter/cli.rb
@@ -10,21 +10,37 @@ module DailyReporter
     def create
       Dotenv.load
 
+      # Set TogglV8::API
       toggl_api    = TogglV8::API.new(ENV["TOGGL_API_TOKEN"])
       user         = toggl_api.me(all=true)
       workspaces   = toggl_api.my_workspaces(user)
       workspace_id = workspaces.first['id']
 
+      # Set TigglV8::ReportAPI
       reports = TogglV8::ReportsV2.new(api_token: ENV["TOGGL_API_TOKEN"])
-      begin
-        reports.summary
-      rescue Exception => e
-        puts e.message      # workspace_id is required
-      end
       reports.workspace_id  = workspace_id
-      summary               = reports.summary
-      puts "Generating summary JSON..."
-      puts JSON.pretty_generate(summary)
+
+      # Fetch and Print
+      today_str = (Time.now - 10000).strftime("%F")
+      params = {since: today_str, until: today_str}
+      summary = reports.summary('', params)
+      result = summary[0]["items"].each_with_object({}) do |item, hash|
+        title = item["title"]["time_entry"]
+        hour, minute = milliseconds_to_hour_and_minute(item["time"])
+        hash[title] = "#{hour}h #{minute}min"
+        if hour.zero?
+          puts "- #{title} (#{minute}min)" 
+        else
+          puts "- #{title} (#{hour}h#{minute}min)"
+        end
+      end
     end
+
+    no_commands do
+      def milliseconds_to_hour_and_minute(msec)
+        minute = msec/1000/60
+        return minute.div(60), minute.modulo(60)
+      end
+    end 
   end
 end
